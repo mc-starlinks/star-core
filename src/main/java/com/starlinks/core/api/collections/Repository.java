f1b79982d6e9bf3b1c7bf881b22556e2f5c13731
zkingboos <kingboosoficial@gmail.com>
2020-04-20 06:48:26 -0300

    added repository methods

--- src/main/java/com/starlinks/core/api/collections/Repository.java
+++ src/main/java/com/starlinks/core/api/collections/Repository.java
@@ -1,9 +1,21 @@
 package com.starlinks.core.api.collections;
 
+import com.starlinks.core.api.database.StarDatabaseProvider;
+
+import java.util.function.Supplier;
+
 public interface Repository<K, T> {
 
-    T find(K key);
-    void set(T instance);
-    void update();
+    StarDatabaseProvider getProvider();
 
+    void createProcedures(StarDatabaseProvider provider);
+
+    T find(K key);
+    void insert(T instance);
+    void update(T instance);
+
+    void insertOrUpdate(T instance);
+    void findOrInsert(K key, Supplier<T> supplier);
+
+    void findAndDelete(K key);
 }
